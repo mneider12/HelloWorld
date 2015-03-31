c95db6caa50efb4c9025338de8b8afdae4c18f98
Mark Nyder <mneider.virginia@gmail.com>
2015-03-30 22:41:16 -0500

    Start to add internet querying

--- app/src/main/java/nydev/com/helloworld/MainActivity.java
+++ app/src/main/java/nydev/com/helloworld/MainActivity.java
@@ -3,10 +3,12 @@ package nydev.com.helloworld;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +17,15 @@ import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends ActionBarActivity {
+    private static final String DEBUG_TAG = "Hello_World";
+    private PlaceholderFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +33,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment(),"place_holder")
+                    .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        PlaceholderFragment fragment = (PlaceholderFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.container);
     }
  
 
@@ -56,8 +67,6 @@ public class MainActivity extends ActionBarActivity {
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        PlaceholderFragment fragment = (PlaceholderFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.container);
         if (networkInfo != null && networkInfo.isConnected()) {
             fragment.setText("Connected");
         } else {
@@ -87,4 +96,57 @@ public class MainActivity extends ActionBarActivity {
         }
 
     }
+
+    private class getFinanceDataTask extends AsyncTask <String, Void, String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve webpage";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            fragment.setText(result);
+        }
+
+        // Given a URL, establishes an HttpUrlConnection and retrieves
+// the web page content as a InputStream, which it returns as
+// a string.
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+    }
+
+
 }
+
